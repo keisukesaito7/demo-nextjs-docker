b20f15651507df02dfbc7036a4399129d466c5aa
keisukesaito7 <keisukesaito7@gmail.com>
2021-02-14 11:52:23 +0900

    linterを実行（保存アクション）

--- demo-app/pages/index.tsx
+++ demo-app/pages/index.tsx
@@ -1,13 +1,8 @@
 import { NextPage } from 'next'
-import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      // ...
-    </div>
-  )
+  return <div className={styles.container}></div>
 }
 
 export default Home
