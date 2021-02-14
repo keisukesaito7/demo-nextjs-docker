d33e2748057a889dcd415347d77420cd766e4370
keisukesaito7 <keisukesaito7@gmail.com>
2021-02-14 11:12:11 +0900

    css -> scss

--- demo-app/pages/_app.tsx
+++ demo-app/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { AppProps } from 'next/app'
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
--- demo-app/pages/index.tsx
+++ demo-app/pages/index.tsx
@@ -1,11 +1,11 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-    // ...
+      // ...
     </div>
   )
 }
